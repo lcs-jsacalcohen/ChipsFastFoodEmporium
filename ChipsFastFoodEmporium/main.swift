ec20adfaa54505a01a41a33683f925095cb21cc9
Russell Gordon <jsacalcohen@lcs.on.ca>
2018-04-26 15:07:34 -0400

    Arrays

--- ChipsFastFoodEmporium/main.swift
+++ ChipsFastFoodEmporium/main.swift
@@ -38,6 +38,92 @@ while 1 == 1 {
     
 }
 
+//drink made
+var drinkChoiceMade = 0
+while 1 == 1 {
+    
+    // Prompt the user
+    print("Please enter a drink choice:")
+    
+    // Test #1: Get input and make sure it is not nil
+    guard let inputGiven = readLine() else {
+        // if nil, ask again
+        continue
+    }
+    
+    // Test #2: Can it be an integer?
+    guard let integerGiven = Int(inputGiven) else {
+        // if it can't be an integer, prompt again
+        continue
+    }
+    
+    // Test #3: Is it within the range 1 to 4?
+    
+    
+    // If we got here, it is good input
+    drinkChoiceMade = integerGiven
+    break
+
+}
+
+//Side Order
+var sideOrderMade = 0
+while 1 == 1 {
+    
+    // Prompt the user
+    print("Please enter a side order:")
+    
+    // Test #1: Get input and make sure it is not nil
+    guard let inputGiven = readLine() else {
+        // if nil, ask again
+        continue
+    }
+    
+    // Test #2: Can it be an integer?
+    guard let integerGiven = Int(inputGiven) else {
+        // if it can't be an integer, prompt again
+        continue
+    }
+    
+    // Test #3: Is it within the range 1 to 4?
+  
+    
+    
+    // If we got here, it is good input
+    sideOrderMade = integerGiven
+    break
+    
+}
+
+//desert
+var dessertChoiceMade = 0
+while 1 == 1 {
+    
+    // Prompt the user
+    print("Please enter a dessert choice:")
+    
+    // Test #1: Get input and make sure it is not nil
+    guard let inputGiven = readLine() else {
+        // if nil, ask again
+        continue
+    }
+    
+    // Test #2: Can it be an integer?
+    guard let integerGiven = Int(inputGiven) else {
+        // if it can't be an integer, prompt again
+        continue
+    }
+    
+    // Test #3: Is it within the range 1 to 4?
+    
+    
+    // If we got here, it is good input
+    dessertChoiceMade = integerGiven
+    break
+    
+}
+
+
 //Array For burger Choices
 var burgerChoices = Array(repeating: 0, count: 5)
 burgerChoices[0] = 0 // never happens
@@ -72,14 +158,14 @@ dessertChoices[4] = 0 // no dessert
 
 // Faking input until we write the code to actually take the input from the user
 //let burgerChoiceMade = 3
-let drinkChoiceMade = 3
-let sideOrderChoiceMade = 1
-let dessertChoiceMade = 2
+//let drinkChoiceMade = 4
+//let sideOrderChoiceMade = 4
+//let dessertChoiceMade = 4
 
 
 // PROCESS
 // Implement the primary logic of the problem here
-var calories = burgerChoices[burgerChoiceMade] + drinkChoices[drinkChoiceMade] + sideOrders[sideOrderChoiceMade] + dessertChoices[dessertChoiceMade]
+var calories = burgerChoices[burgerChoiceMade] + drinkChoices[drinkChoiceMade] + sideOrders[sideOrderMade] + dessertChoices[dessertChoiceMade]
 
 print("Total calories are: \(calories)")
 // OUTPUT
@@ -87,6 +173,7 @@ print("Total calories are: \(calories)")
 
 
 // Report results to the user here
+
 
 
 
